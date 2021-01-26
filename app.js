a86455a85e95c7a125d066bc1b438d5082664bfd
Luk1Pr0 <lpytel16@gmail.com>
2021-01-26 16:12:33 +0100

    Minor changes to the logo/favicon, styling and added more functionality

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const navIcon = document.getElementById('navIcon');
 const navMenu = document.getElementById('navMenu');
+const header = document.querySelector('header');
+const section1 = document.querySelector('.section--1');
+
+const mobileScreen = 1023;
 
 // Close or open nav menu
 const toggleNav = () => {
@@ -7,5 +11,29 @@ const toggleNav = () => {
 	navIcon.classList.toggle('burger__cross');
 }
 
+// Attach the nav menu to the window when user scrolls
+const showNavMenu = (e) => {
+	// If screen size
+	if (window.innerWidth > mobileScreen) {
+		const scrollPos = window.pageYOffset;
+		const sectionOffset = section1.getBoundingClientRect().top;
+		const navHeight = navMenu.getBoundingClientRect().height;
+		// If scrolls pos is more than 1px hide navbar
+		if (scrollPos > 1) {
+			navMenu.classList.add('nav__hidden');
+			// Once section offset is less than the navHeight, stick the navbar
+			if (sectionOffset < navHeight * 1.2) {
+				navMenu.classList.add('nav__sticky');
+			} else {
+				navMenu.classList.remove('nav__sticky');
+			}
+		} else {
+			navMenu.classList.remove('nav__hidden');
+		}
+	}
+}
+
+
 // Event listeners
 navIcon.addEventListener('click', toggleNav);
+window.addEventListener('scroll', showNavMenu);
