e3ae809d5dfc2826ade551e9b93685c9cc72bb5f
Luk1Pr0 <lpytel16@gmail.com>
2021-01-30 16:37:44 +0100

    Added functionality to filter through the members depending on their role

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const header = document.querySelector('header');
 const section1 = document.querySelector('.section--1');
 const cardsContainer = document.querySelector('.section--services');
 
+// Team page
+const filterSelector = document.getElementById('filterMembers');
+const members = document.querySelectorAll('.member__container');
+
 const mobileScreen = 1024;
 
 // Close or open nav menu
@@ -37,6 +41,36 @@ const showNavMenu = () => {
 	}
 }
 
+// When the select input changes, display the user depending on their role
+const displayFiltered = (e) => {
+	// Value of the selected option
+	let value = e.target.value;
+
+	members.forEach(member => {
+		// Role of each individual user
+		let role = member.children[2].textContent.toLowerCase();
+
+		// Set smooth transition
+		member.style.transition = 'all 0.5s ease';
+
+		// If role does not include the selected value then hide the member
+		if (!role.includes(value)) {
+			member.style.opacity = '0';
+			member.style.transform = 'scale(0)';
+			// Don't display the member after 500ms
+			setTimeout(() => member.style.display = 'none', 500);
+		} else {
+			setTimeout(() => member.style.display = 'flex', 500);
+			// Display member after the display is set to flex
+			setTimeout(() => {
+				member.style.opacity = '1';
+				member.style.transform = 'scale(1)';
+			}, 600);
+		}
+	});
+}
+
 // Event listeners
 navIcon.addEventListener('click', toggleNav);
 window.addEventListener('scroll', showNavMenu);
+filterSelector.addEventListener('change', displayFiltered);
